371f44ef570483f50372be91684467467028d600
kanekotic <alvarojosepl@gmail.com>
2019-04-09 21:08:21 +0200

    added the integration test

--- pkg/integration_test.go
+++ pkg/integration_test.go
@@ -1,12 +1,16 @@
 package pkg
 
 import (
-	"fmt"
+	"github.com/feaggle/feaggle/pkg/drivers"
+	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
 )
 
 
 func Test_Integration_Toggle(t *testing.T) {
-	feaggle := FeaggleBuilder{}.Create()
-	fmt.Println("Value:", feaggle.IsActive("pepe"))
+
+	os.Setenv("FAKE_TOGGLE_ENVIRONMENT_VARIABLE", "true")
+	feaggle := FeaggleBuilder{}.AddDriver(drivers.ReleaseEnvironmentDriver{}).Create()
+	assert.True(t, feaggle.IsActive("FAKE_TOGGLE_ENVIRONMENT_VARIABLE"), "if is true, returns true")
 }
