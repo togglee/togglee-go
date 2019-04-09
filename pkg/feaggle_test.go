a399d3d92fd37c7524d77553442f36754e20b9a0
kanekotic <alvarojosepl@gmail.com>
2019-04-09 20:34:04 +0200

    added full set of test for feaggle

--- pkg/feaggle_test.go
+++ pkg/feaggle_test.go
@@ -48,3 +48,59 @@ func Test_Toggle_Is_False_If_Exist_In_Driver(t *testing.T) {
 
 	assert.False(t, feaggle.IsActive(name), "is false in driver")
 }
+
+func Test_Toggle_Multiple_Drivers_False(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+	name := faker.UUIDHyphenated()
+	driver := mocks.NewMockDriver(controller)
+	otherDriver := mocks.NewMockDriver(controller)
+	driver.EXPECT().IsActive(name).Return(nil)
+	otherDriver.EXPECT().IsActive(name).Return(boolToPointer(true))
+
+	feaggle := FeaggleBuilder{}.AddDriver(driver).AddDriver(otherDriver).Create()
+
+	assert.True(t, feaggle.IsActive(name), "is false in driver")
+}
+
+func Test_Toggle_Multiple_Drivers_False_Not_Exist(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+	name := faker.UUIDHyphenated()
+	driver := mocks.NewMockDriver(controller)
+	otherDriver := mocks.NewMockDriver(controller)
+	driver.EXPECT().IsActive(name).Return(nil)
+	otherDriver.EXPECT().IsActive(name).Return(nil)
+
+	feaggle := FeaggleBuilder{}.AddDriver(driver).AddDriver(otherDriver).Create()
+
+	assert.False(t, feaggle.IsActive(name), "is false in driver")
+}
+
+func Test_Toggle_Multiple_Drivers_False_Exist(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+	name := faker.UUIDHyphenated()
+	driver := mocks.NewMockDriver(controller)
+	otherDriver := mocks.NewMockDriver(controller)
+	driver.EXPECT().IsActive(name).Return(boolToPointer(true))
+	otherDriver.EXPECT().IsActive(name).Return(boolToPointer(false))
+
+	feaggle := FeaggleBuilder{}.AddDriver(driver).AddDriver(otherDriver).Create()
+
+	assert.False(t, feaggle.IsActive(name), "is false in driver")
+}
+
+func Test_Toggle_Multiple_Drivers_False_Exist_Does_Not_Call_Next(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+	name := faker.UUIDHyphenated()
+	driver := mocks.NewMockDriver(controller)
+	otherDriver := mocks.NewMockDriver(controller)
+	driver.EXPECT().IsActive(name).Return(boolToPointer(false))
+	otherDriver.EXPECT().IsActive(name).Return(boolToPointer(true)).Times(0)
+
+	feaggle := FeaggleBuilder{}.AddDriver(driver).AddDriver(otherDriver).Create()
+
+	assert.False(t, feaggle.IsActive(name), "is false in driver")
+}
